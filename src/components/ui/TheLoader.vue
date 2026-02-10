75d09124074cbfa5d3e31b3cedcc9be3be9d97d7
Yura Mishin <robothot@yandex.ru>
2026-02-10 20:07:17 +0300

    Изменил внешний вид лоадера

--- src/components/ui/TheLoader.vue
+++ src/components/ui/TheLoader.vue
@@ -1,16 +1,9 @@
 <template>
-  <div class="loader-wrapper">
-    <span class="loader-letter">П</span>
-    <span class="loader-letter">о</span>
-    <span class="loader-letter">д</span>
-    <span class="loader-letter">о</span>
-    <span class="loader-letter">ж</span>
-    <span class="loader-letter">д</span>
-    <span class="loader-letter">и</span>
-    <span class="loader-letter">т</span>
-    <span class="loader-letter">е</span>
-
-    <div class="loader"></div>
+  <div class="backdrop">
+    <div class="loader">
+    <div class="loader_cube loader_cube--color"></div>
+    <div class="loader_cube loader_cube--glowing"></div>
+  </div>
   </div>
 </template>
 
@@ -26,312 +19,55 @@ export default {
 </script>
 
 <style scoped>
-.loader{
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 60px;
-  opacity: 0;
+.loader {
+  width: 100px;
+  height: 100px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
 }
-.loader span {
+
+.backdrop {
+  content: "";
   position: absolute;
-  left: 42%;
-  top: 42%;
-  width: 10px;
-  height: 10px;
-  border-radius: 100px;
-  background: linear-gradient(135deg, #6a5acd, #1e90ff);
-  animation: whirlpool 3s infinite;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
 }
 
-/* размещаем круги по окружности */
-.loader span:nth-child(1) {
-  animation: whirlpool1 3s ease-in-out infinite;
-  animation-delay: 0s;
+.loader_cube {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 30px;
 }
 
-.loader span:nth-child(2) {
-  animation: whirlpool2 3s ease-in-out infinite;
-  animation-delay: 0.1s;
+.loader_cube--glowing {
+  z-index: 2;
+  background-color: rgba(255, 255, 255, 0.2);
+  border: 2px solid rgba(255, 255, 255, 0.3);
 }
 
-.loader span:nth-child(3) {
-  animation: whirlpool3 3s ease-in-out infinite;
-  animation-delay: 0.2s;
+.loader_cube--color {
+  z-index: 1;
+  filter: blur(2px);
+  background: linear-gradient(135deg, #1afbf0, #da00ff);
+  animation: loadtwo 2.5s ease-in-out infinite;
 }
 
-.loader span:nth-child(4) {
-  animation: whirlpool4 3s ease-in-out infinite;
-  animation-delay: 0.3s;
-}
-
-.loader span:nth-child(5) {
-  animation: whirlpool5 3s ease-in-out infinite;
-  animation-delay: 0.4s;
-}
-
-.loader span:nth-child(6) {
-  animation: whirlpool6 3s ease-in-out infinite;
-  animation-delay: 0.5s;
-}
-
-.loader span:nth-child(7) {
-  animation: whirlpool7 3s ease-in-out infinite;
-  animation-delay: 0.6s;
-}
-
-.loader span:nth-child(8) {
-  animation: whirlpool8 3s ease-in-out infinite;
-  animation-delay: 0.7s;
-}
-
-.loader span:nth-child(9) {
-  animation: whirlpool9 3s ease-in-out infinite;
-  animation-delay: 0.8s;
-}
-
-.loader span:nth-child(10) {
-  animation: whirlpool10 3s ease-in-out infinite;
-  animation-delay: 0.9s;
-}
-
-.loader span:nth-child(11) {
-  animation: whirlpool11 3s ease-in-out infinite;
-  animation-delay: 1s;
-}
-
-.loader span:nth-child(12) {
-  animation: whirlpool12 3s ease-in-out infinite;
-  animation-delay: 1.1s;
-}
-
-@keyframes whirlpool1 {
-  0% {
-    transform: scale(1) translate(30px, 0);
-    opacity: 1;
-  }
-
+@keyframes loadtwo {
   50% {
-    transform: scale(1) translate(0, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: scale(1) translate(30px, 0);
-    opacity: 1;
-  }
-}
-
-@keyframes whirlpool2 {
-  0% {
-    transform: translate(26px, 15px);
-    opacity: 1;
-  }
-
-  50% {
-    transform: translate(0, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: translate(26px, 15px);
-    opacity: 1;
-  }
-}
-
-@keyframes whirlpool3 {
-  0% {
-    transform: translate(15px, 26px);
-    opacity: 1;
-  }
-
-  50% {
-    transform: translate(0, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: translate(15px, 26px);
-    opacity: 1;
-  }
-}
-
-@keyframes whirlpool4 {
-  0% {
-    transform: translate(0px, 30px);
-    opacity: 1;
-  }
-
-  50% {
-    transform: translate(0, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: translate(0px, 30px);
-    opacity: 1;
-  }
-}
-
-@keyframes whirlpool5 {
-  0% {
-    transform: translate(-15px, 26px);
-    opacity: 1;
-  }
-
-  50% {
-    transform: translate(0%, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: translate(-15px, 26px);
-    opacity: 1;
-  }
-}
-
-@keyframes whirlpool6 {
-  0% {
-    transform: translate(-26px, 15px);
-    opacity: 1;
-  }
-
-  50% {
-    transform: translate(0%, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: translate(-26px, 15px);
-    opacity: 1;
-  }
-}
-
-@keyframes whirlpool7 {
-  0% {
-    transform: translate(-30px, 0px);
-    opacity: 1;
-  }
-
-  50% {
-    transform: translate(0%, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: translate(-30px, 0px);
-    opacity: 1;
-  }
-}
-
-@keyframes whirlpool8 {
-  0% {
-    transform: translate(-26px, -15px);
-    opacity: 1;
-  }
-
-  50% {
-    transform: translate(0%, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: translate(-26px, -15px);
-    opacity: 1;
-  }
-}
-
-@keyframes whirlpool9 {
-  0% {
-    transform: translate(-15px, -26px);
-    opacity: 1;
-  }
-
-  50% {
-    transform: translate(0%, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: translate(-15px, -26px);
-    opacity: 1;
-  }
-}
-
-@keyframes whirlpool10 {
-  0% {
-    transform: translate(0, -30px);
-    opacity: 1;
-  }
-
-  50% {
-    transform: translate(0%, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: translate(0, -30px);
-    opacity: 1;
-  }
-}
-
-@keyframes whirlpool11 {
-  0% {
-    transform: translate(15px, -26px);
-    opacity: 1;
-  }
-
-  50% {
-    transform: translate(0%, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: translate(15px, -26px);
-    opacity: 1;
-  }
-}
-
-@keyframes whirlpool12 {
-  0% {
-    transform: translate(26px, -15px);
-    opacity: 1;
-  }
-
-  50% {
-    transform: translate(0%, 0);
-    opacity: 0.2;
-  }
-
-  100% {
-    transform: translate(26px, -15px);
-    opacity: 1;
-  }
-}
-
-@keyframes morphing {
-  0% {
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-  }
-
-  25% {
-    width: 100%;
-    height: 10px;
-    border-radius: 40px;
-  }
-
-  75% {
-    width: 10px;
-    height: 100%;
-    border-radius: 40px;
-  }
-
-  100% {
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
+    transform: rotate(-80deg);
   }
 }
 </style>
